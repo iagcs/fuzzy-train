4c3484ea5ddc015f96a3e5308503dfa2dd24eb49
Iago Souto <iago3220@gmail.com>
2024-12-07 00:40:02 -0300

    Improvements on user preferences logic

--- database/migrations/2024_12_05_033825_create_news_preferences_table.php
+++ database/migrations/2024_12_05_033825_create_news_preferences_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('news_preferences', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->uuid('id')->primary()->default(\DB::raw('uuid_generate_v4()'));
 
-            $table->uuidMorphs('preferred');
+            $table->uuidMorphs('news_preference');
             $table->foreignUuid('user_id')->constrained('users');
 
             $table->timestamps();
